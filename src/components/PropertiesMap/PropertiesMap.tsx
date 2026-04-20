44915c933561776498a0a7b0664cc8957a4686d9
Erwin Jimenez <erwinjv98@gmail.com>
2026-04-20 13:52:45 -0400

    Delete map pointers labels

--- src/components/PropertiesMap/PropertiesMap.tsx
+++ src/components/PropertiesMap/PropertiesMap.tsx
@@ -58,6 +58,7 @@ export default function PropertiesMap({
             <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
           </svg>
           <div style="
+              display: none;
             position: absolute;
             bottom: -25px;
             left: 50%;
@@ -101,6 +102,7 @@ export default function PropertiesMap({
             <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7z"/>
           </svg>
           <div style="
+            display: none;  
             position: absolute;
             bottom: -22px;
             left: 50%;
